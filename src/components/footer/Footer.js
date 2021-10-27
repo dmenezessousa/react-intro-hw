42e1cd1cba3c2d2e1a6a80670c700fc8db9a5e35
KheSanh-Code <dmenezessousaa1@gmail.com>
2021-10-27 12:37:07 -0400

    Added Header and Footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import "./Footer.css";
+
+
+export class Footer extends Component {
+    render() {
+        return (
+            <div  className="footer-div">
+                <h2 className="footer-title">@Copywrite Diego Sousa 2021</h2>
+            </div>
+        )
+    }
+}
+
+export default Footer
---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./Header.css";
+
+export class Header extends Component {
+    render() {
+        return (
+            <div className="header-div">
+                <h1 className="header-title">Diego</h1>
+            </div>
+        )
+    }
+}
+
+export default Header;
